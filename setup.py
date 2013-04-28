6b4699b6cdd544533cb24c041cde818e22eae8a9
Lloyd Konneker <bootch@nc.rr.com>
2013-04-28 11:57:46 -0400

    Python disutils distribution

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='documentStyle',
+      version='0.1',
+      description='Document Styling Classes',
+      author='Lloyd Konneker',
+      author_email='bootch@nc.rr.com',
+      url='https://github.com/bootchk/documentStyle',
+      packages=['documentStyle', 
+                'documentStyle.formation',
+                'documentStyle.formation.instrumentFormation',
+                'documentStyle.formation.instrumentFormation.textFormation', 
+                'documentStyle.instrument',
+                'documentStyle.styleSheet',
+                'documentStyle.styling',
+                'documentStyle.userInterface',
+                'documentStyle.userInterface.layout',
+                'documentStyle.userInterface.styleDialog',
+                'documentStyle.userInterface.stylePickerWidget',
+                ],
+     )
