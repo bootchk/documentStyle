560c9831b3c98c928c1e901548be23a769fa1c1a
Lloyd Konneker <bootch@nc.rr.com>
2013-09-01 05:48:31 -0400

    New adaptor for setForegroundColor in CharacterFormation

--- documentStyle/formation/instrumentFormation/textFormation/characterFormation.py
+++ documentStyle/formation/instrumentFormation/textFormation/characterFormation.py
@@ -15,60 +15,67 @@ class CharacterFormation(TextFormation):
   Styling attributes of characters.
   
   Specialize to Qt <QTextCharFormat>
+  
+  Notes:
+  Color property is foreground.
+  We choose not to offer background color as a style property.
+  Background color applied to text document gives jaggy blocks of color background.
+  Probably better to offer a property on a background layer (symmetric balloon) behind the text. 
+  Background color defaults to transparent.
   '''
   
   def __init__(self, parentSelector):
     InstrumentFormation.__init__(self, name="Character", parentSelector=parentSelector)
     self.instrument = QTextCharFormat()
     self.styleProperties=[ColorStyleProperty("Color", 
-                                             self.instrument.foreground().setColor,  self.selector,
+                                             self.setForegroundColor,  
+                                             self.selector,
                                              default=self.instrument.foreground().color(),
                                              minimum=0, maximum=0), 
                       FontStyleProperty("Font", self.instrument.setFont, self.selector,
                                         default=FontStyleWrapper(self.instrument.font()),
                                         minimum=0, maximum=0,)]
                                         ## model = FontModel),]
-  
+
   
   def applyToCursor(self, cursor):
     ''' Effect deferred method. '''
-    
-    '''
-    For some reason, StyleProperty calling the setter doesn't work.
-    The QBrush object seems to change.
-    (May be a bug in PySide?? or my improper understanding of it.)
-    (Elsewhere, StyleProperty calling the setter does seem to work.)
-    So this hack sets the QBrush at the last moment, using cached value of the StyleProperty.
-    '''
-    brush = self.instrument.foreground()
-    brush.setColor( self.styleProperties[0].get() ) # HACK: hardcoded
-    brush.setStyle(Qt.SolidPattern) # HACK: doesn't allow brush pattern editable by user
-    self.instrument.setForeground(brush)
-  
-    
-    """
-    # debugging why QBrush setter doesn't work.
-    #print "applyToCursor: Property setter", self.styleProperties[0].setter
-    #print "applyToCursor setter: ", brush.setColor
-    #print "applyToCursor: color", brush.color()
-    #print "applyToCursor: brushStyle", brush.style()
-    #print "applyToCursor:  brush", brush
-    #print "applyToCursor: brush again", brush
-    """
-    
     '''
     Alternatively, setCharFormat.
     Merging leaves some style unchanged, e.g. block format if char format is being merged?
     '''
     cursor.mergeCharFormat(self.instrument)
     
+    
+  def setForegroundColor(self, color):
+    '''
+    Setter for foreground color.
+    
+    Adapter: Qt has no direct setter for this property.
+    Indirect: get brush, set color on brush, set brush.
+    Get brush gets a copy of brush that must be reset onto the format.
+    Just :get brush, set color on brush: has no effect.
+    '''
+    brush = self.instrument.foreground()
+    brush.setColor(color)
+    brush.setStyle(Qt.SolidPattern) # HACK: doesn't allow brush pattern editable by user
+    self.instrument.setForeground(brush)
+    
+    
+  # No setBackgroundColor(), see notes in class docstring
+    
 '''
+At one stage of development,
+the setter was self.instrument.foreground().setColor.
+But that apparently justs sets color on a brush copy, not affecting text.
+So applyToCursor was calling setForegroundColor()
+(and it was resetting brush.)
+
+
 Notes:
 
 TODO instrument.setTextOutline(QPen) another StyleProperty ???
 
 TODO brush.setStyle(Qt.SolidPattern) another StyleProperty so can have non-filled Shapes or non-filled Text ???
-
-Scraps:
 
 '''
