4a12c3c8f7ba5f558e980b2700f80bfc0117004f
bootchk <bootch@nc.rr.com>
2014-07-08 18:21:50 -0400

    Japanese translation file missed on previous commit.

---
+++ resources/translations/documentStyle_ja.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>Translations</name>
+    <message>
+        <location filename="../../documentStyle/config.py" line="36"/>
+        <source>Any</source>
+        <translatorcomment>Whichever</translatorcomment>
+        <translation>すべて</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="39"/>
+        <source>Line</source>
+        <translatorcomment>drawn mark</translatorcomment>
+        <translation>線</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="40"/>
+        <source>Shape</source>
+        <translation>形状</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="41"/>
+        <source>Text</source>
+        <translation>テキスト</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="42"/>
+        <source>Pixmap</source>
+        <translatorcomment>pictures</translatorcomment>
+        <translation>画像</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="50"/>
+        <source>Pen</source>
+        <translation>ペン</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="46"/>
+        <source>Brush</source>
+        <translatorcomment>n. artist painting</translatorcomment>
+        <translation>筆</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="47"/>
+        <source>Block</source>
+        <translatorcomment>section of text</translatorcomment>
+        <translation>段落</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="48"/>
+        <source>Character</source>
+        <translatorcomment>letter of alphabet</translatorcomment>
+        <translation>文字</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="49"/>
+        <source>Opacity</source>
+        <translatorcomment>lack of transparency</translatorcomment>
+        <translation>不透明、不透過度、混濁</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="51"/>
+        <source>Effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="54"/>
+        <source>Frame</source>
+        <translatorcomment>ornamental strip</translatorcomment>
+        <translation>枠</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="55"/>
+        <source>Ground</source>
+        <translatorcomment>scene in the rear</translatorcomment>
+        <translation>背景</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="59"/>
+        <source>Color</source>
+        <translation>カラー</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="62"/>
+        <source>Width</source>
+        <translation>幅</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="63"/>
+        <source>Style</source>
+        <translatorcomment>artistic features</translatorcomment>
+        <translation>描出法</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="65"/>
+        <source>Pattern</source>
+        <translatorcomment>design</translatorcomment>
+        <translation>柄</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="67"/>
+        <source>Font</source>
+        <translation>フォント</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="69"/>
+        <source>Aligned</source>
+        <translatorcomment>line up with</translatorcomment>
+        <translation>～と手を組む、提携する</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="70"/>
+        <source>Spacing</source>
+        <translation>間隔（をあけること）</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="82"/>
+        <source>Solid</source>
+        <translatorcomment>without holes</translatorcomment>
+        <translation>中身の詰まった</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="77"/>
+        <source>Dashed</source>
+        <translatorcomment>symbol -</translatorcomment>
+        <translation>ダッシュ</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="78"/>
+        <source>Dotted</source>
+        <translatorcomment>symbol .</translatorcomment>
+        <translation>ドット</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="79"/>
+        <source>DashDot</source>
+        <translatorcomment>dash,dot</translatorcomment>
+        <translation>ダッシュ,ドット</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="81"/>
+        <source>None</source>
+        <translation>なし</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="83"/>
+        <source>Half dense</source>
+        <translatorcomment>light and dark of middle value</translatorcomment>
+        <translation>ハーフトーンの、中間調の</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="84"/>
+        <source>Quadrule</source>
+        <translatorcomment>give checked pattern to</translatorcomment>
+        <translation>格子状にする</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="85"/>
+        <source>Crosshatch</source>
+        <translatorcomment>thatch roof covering</translatorcomment>
+        <translation>草、わら、シュロ</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="87"/>
+        <source>Single</source>
+        <translation>ひとつ</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="88"/>
+        <source>1.5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="89"/>
+        <source>Double</source>
+        <translation>2つの</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="91"/>
+        <source>Left</source>
+        <translatorcomment>left side</translatorcomment>
+        <translation>←</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="92"/>
+        <source>Right</source>
+        <translation>→</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="93"/>
+        <source>Center</source>
+        <translation>真ん中</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="94"/>
+        <source>Justify</source>
+        <translatorcomment>text: align</translatorcomment>
+        <translation>～の行末を揃える</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="97"/>
+        <source>App Style Defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="100"/>
+        <source>App</source>
+        <translatorcomment>abbr: computer application</translatorcomment>
+        <translation>アプリケーション</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="101"/>
+        <source>User</source>
+        <translation>ユーザー</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="102"/>
+        <source>Doc</source>
+        <translatorcomment>computer document n</translatorcomment>
+        <translation>ドキュメント</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="103"/>
+        <source>Tool Style</source>
+        <translatorcomment>tool, style</translatorcomment>
+        <translation>道具,描出法</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="104"/>
+        <source>Element Style</source>
+        <translatorcomment>segment, style</translatorcomment>
+        <translation>部分,描出法</translation>
+    </message>
+    <message>
+        <location filename="../../documentStyle/config.py" line="105"/>
+        <source>Style Sheet</source>
+        <translatorcomment>style, exacting description</translatorcomment>
+        <translation>描出法, 詳細</translation>
+    </message>
+</context>
+</TS>
